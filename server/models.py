b8c7cd6b53150175f47cff534ad5579960b93f1c
rovenelabanga <rovenelabanga2001@gmail.com>
2025-01-28 13:49:45 +0300

    adds the budget and userbudget model

--- server/models.py
+++ server/models.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
+from datetime import datetime
 
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -20,3 +21,32 @@ class User(db.Model, SerializerMixin):
 
 
 class Transaction(db.Model, SerializerMixin):
+    __tablename__ = 'transactions'
+
+    id = db.Column(db.Integer, primary_key=True)
+    transaction_type = db.Column(db.String, nullable=False)
+    category = db.Column(db.String(50), nullable=False)
+    date = db.Column(db.Date, nullable=False)
+    description = db.Column(db.String(100), nullable=False)
+    amount = db.Column(db.Date, nullable=False)
+
+    def __repr__(self):
+        return f'<Transaction: {self.id}, type:{self.transaction_type}, category: {self.category}, date: {self.date}>, amount: {self.amount}, description: {self.description}'
+
+class Budget(db.Model, SerializerMixin):
+    __tablename__ = 'budgets'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    amount = db.Column(db.Integer, nullable=False)
+    start_date = db.Column(db.Date, nullable=False)
+    end_date = db.Column(db.Date, nullable=False)
+
+    def __repr__(self):
+        return f'<Budget: {self.id}, name: {self.name}, amount: {self.amount}, start_date: {self.start_date}, end_date: {self.end_date}>'
+
+class UserBudget(db.Model, SerializerMixin):
+    __tablename__ = 'userbudget'
+
+    id = db.Column(db.Integer, primary_key=True)
+    contribution_amount = db.Column(db.Integer, nullable=False)
